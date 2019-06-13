8e46a8c2ef9e676d11709c5ccb79b2937fab77ea
akkiboeing <41015771+akkiboeing@users.noreply.github.com>
2019-06-13 18:20:10 +0530

    Update map.js

--- webUI/assets/js/map.js
+++ webUI/assets/js/map.js
@@ -1,7 +1,7 @@
 function showPosition(position) {
   var latlon = position.coords.latitude + "," + position.coords.longitude;
 
-  var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key=AIzaSyBRDxyphjOuTjf8tTmchDubiYlpmSyLG5Y";
+  var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key=yourAPIkey";
 
   document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
 }
